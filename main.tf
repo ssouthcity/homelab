88ea11b6c99b4908f8b3fa997e33613883f9a129
Stian Sørby <51554341+ssouthcity@users.noreply.github.com>
2024-07-13 16:30:06 +0000

    added sonarr

--- main.tf
+++ main.tf
@@ -5,8 +5,56 @@ resource "kubernetes_namespace" "servarr" {
   }
 }
 
+resource "kubernetes_persistent_volume_claim" "servarr_tv" {
+  metadata {
+    name      = "servarr-tv"
+    namespace = kubernetes_namespace.servarr.metadata.0.name
+  }
+
+  spec {
+    storage_class_name = "local-path"
+    access_modes       = ["ReadWriteOnce"]
+
+    resources {
+      requests = {
+        storage = "128Gi"
+      }
+    }
+  }
+
+  wait_until_bound = false
+}
+
+resource "kubernetes_persistent_volume_claim" "servarr_downloads" {
+  metadata {
+    name      = "servarr-downloads"
+    namespace = kubernetes_namespace.servarr.metadata.0.name
+  }
+
+  spec {
+    storage_class_name = "local-path"
+    access_modes       = ["ReadWriteOnce"]
+
+    resources {
+      requests = {
+        storage = "128Gi"
+      }
+    }
+  }
+
+  wait_until_bound = false
+}
+
 module "prowlarr" {
   source = "./modules/prowlarr"
 
   namespace = kubernetes_namespace.servarr.metadata.0.name
+}
+
+module "sonarr" {
+  source = "./modules/sonarr"
+
+  namespace        = kubernetes_namespace.servarr.metadata.0.name
+  volume_downloads = kubernetes_persistent_volume_claim.servarr_downloads.metadata.0.name
+  volume_tv        = kubernetes_persistent_volume_claim.servarr_tv.metadata.0.name
 }
